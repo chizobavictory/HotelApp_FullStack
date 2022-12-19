1c11e6d7730a732cac0b797fa8649b228ce603c9
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-19 09:46:55 +0100

    created user routes🗺

---
+++ src/routes/users.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+const router = express.Router();
+
+import { loginUser, registerUsers, getUsers, renderRegisterPage, renderLoginPage, renderHomePage, renderListingPage, renderSecListingPage, logout} from "../controller/userController";
+
+
+
+/* GET users listing. */
+router.post('/register', registerUsers)
+router.post('/logins', loginUser)
+router.get('/allusers',getUsers)
+router.post('/logout', logout)
+
+
+// ejs routes
+router.get('/signup', renderRegisterPage);
+router.get('/signin', renderLoginPage);
+router.get('/home', renderHomePage);
+router.get('/listing1', renderListingPage);
+router.get('/listing2', renderSecListingPage)
+
+
+export default router;
