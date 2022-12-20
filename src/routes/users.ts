c6434982d403ee490dc1cd42350a7dc3039ccafa
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-20 21:16:39 +0100

    added finishing touches ✨

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,23 +2,28 @@ import express from "express";
 
 const router = express.Router();
 
-import { loginUser, registerUsers, getUsers, renderRegisterPage, renderLoginPage, renderHomePage, renderListingPage, renderSecListingPage, logout} from "../controller/userController";
-
-
+import {
+  loginUser,
+  registerUsers,
+  getUsers,
+  renderRegisterPage,
+  renderLoginPage,
+  renderListingPage,
+  renderSecListingPage,
+  logout,
+} from "../controller/userController";
 
 /* GET users listing. */
-router.post('/register', registerUsers)
-router.post('/logins', loginUser)
-router.get('/allusers',getUsers)
-router.post('/logout', logout)
-
+router.post("/register", registerUsers);
+router.post("/logins", loginUser);
+router.get("/allusers", getUsers);
+router.post("/logout", logout);
 
 // ejs routes
-router.get('/signup', renderRegisterPage);
-router.get('/signin', renderLoginPage);
-router.get('/home', renderHomePage);
-router.get('/listing1', renderListingPage);
-router.get('/listing2', renderSecListingPage)
+router.get("/signup", renderRegisterPage);
+router.get("/signin", renderLoginPage);
 
+router.get("/listing1", renderListingPage);
+router.get("/listing2", renderSecListingPage);
 
 export default router;
